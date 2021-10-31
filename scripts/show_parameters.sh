692cde7261cbd04b4b50c66a9a2754f1202a1fe2
Leif Eriksen <a-leriksen@ausnetservices.com.au>
2021-10-31 13:16:42 +1100

    strip prefix from name string

--- scripts/show_parameters.sh
+++ scripts/show_parameters.sh
@@ -4,8 +4,10 @@ set euo -pipefail
 
 echo "logic goes here"
 
+prefix="parameter_"
+
 for var in "${!parameter_@}"; do
-    printf '%s=%s\n' "$var" "${!var}"
+    printf '%s=%s\n' ${var#"$prefix"} "${!var}"
 done
 
 exit 0
